979194dac836f9c4c3b0af486e9e192b661103c5
Garrett Koller <gkoller@catalist.us>
2017-08-27 22:45:51 -0400

    The add_account.sh script no longer returns an error code if the required environment variables are not present (for Docker builds)

--- scripts/add_account.sh
+++ scripts/add_account.sh
@@ -2,18 +2,18 @@
 
 # Do sanity checks
 if [ -z "${GDRIVE_ACCOUNT}" ]; then
-    echo "No google drive account given. Exiting !"
-    exit 1
+    echo "No Google Drive account given."
 fi
 
 if [ -z "${GDRIVE_AUTHCODE}" ]; then
-    echo "No google drive authentication code given. Exiting !"
+    echo "No Google Drive authentication code given."
     echo "To get the authenticatiom code, go to http://goo.gl/kTvy0y and follow prompts."
-    exit 1
 fi
 
-mkdir -p "/data/${GDRIVE_ACCOUNT}"
-/usr/bin/insync-headless add_account -a "${GDRIVE_AUTHCODE}" -p "/data/${GDRIVE_ACCOUNT}"
-/usr/bin/insync-headless set_autostart yes
+if [ -n "${GDRIVE_ACCOUNT}" ] && [ -n "${GDRIVE_AUTHCODE}" ]; then
+    mkdir -p "/data/${GDRIVE_ACCOUNT}"
+    /usr/bin/insync-headless add_account -a "${GDRIVE_AUTHCODE}" -p "/data/${GDRIVE_ACCOUNT}"
+    /usr/bin/insync-headless set_autostart yes
+fi
 
 exit 0
